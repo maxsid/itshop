26bfa17926e96e349b94954545a206785ce8e998
Maxim Sidorov <maxim_sidorov@outlook.com>
2020-06-22 01:57:32 +0300

    Change the fake test

--- tests/test_test.py
+++ tests/test_test.py
@@ -3,7 +3,8 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test_fake(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
